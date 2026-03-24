263e02556e4916d603c68ec410c22e13ecca6665
C@:\EM1NAL <emaperry@stupidcat.uk>
2026-03-24 23:24:35 +0000

    Simplified item types

--- worlds/pd2_crimdawn/item_types.py
+++ worlds/pd2_crimdawn/item_types.py
@@ -1,19 +1,7 @@
 from typing import NamedTuple
-from enum import IntEnum
 from BaseClasses import ItemClassification
-
-class itemType(IntEnum):
-    goal = 1
-    progression = 2
-    perkDeck = 3
-    skill = 4
-    weapon = 5
-    filler = 6
-    trap = 7
-    unlock = 8
 
 class itemData(NamedTuple):
     classification: ItemClassification
     count: int
     name: str
-    type: itemType
